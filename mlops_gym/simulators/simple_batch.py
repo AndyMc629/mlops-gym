4d7a19fe0e2159a7957b157b38809184d2ba8a22
AndyMc629 <andrewpmcmahon629@gmail.com>
2022-04-04 21:30:37 +0100

    simple batch simulator ...

---
+++ mlops_gym/simulators/simple_batch.py
@@ -0,0 +1,48 @@
+import mlops_gym
+
+BATCH_SIZE = 10
+
+
+
+
+class SimpleBatchSimulator(mlops_gym.Simulator):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+    def model_predict(self):
+        pass
+
+
+def create_wine_classifier_model():
+    from sklearn.datasets import load_wine
+    import sklearn.metrics
+    import autosklearn.classification
+    from sklearn.model_selection import train_test_split
+
+    automl = autosklearn.classification.AutoSklearnClassifier(
+        time_left_for_this_task=30,#60,
+        per_run_time_limit=2#30
+    )
+
+    # Grab the data
+    wine_data = load_wine()
+    feature_names = wine_data.feature_names
+    X, y = wine_data.data, wine_data.target
+
+    # Make a 70/30 reference/test split
+    X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                    test_size=0.70,
+                                                    random_state=42)
+
+    automl.fit(X_train, y_train, dataset_name='wine')
+
+    print(automl.show_models())
+    print(automl.sprint_statistics())
+    predictions = automl.predict(X_test)
+    sklearn.metrics.accuracy_score(y_test, predictions)
+
+    return automl
+
+
+if __name__ == "__main__":
+    model = create_wine_classifier_model()
+    simple_batch_simulator = SimpleBatchSimulator(model=model)
